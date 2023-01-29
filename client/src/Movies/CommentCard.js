8eb17f6d970b38b9b7c3f590cbc4f05d0402e933
Carrie Miskin <clove40@gmail.com>
2023-01-29 17:26:08 -0500

    added comments_controller and routes
    Co-authored-by: John Ochs <code@johnochs.io>

--- client/src/Movies/CommentCard.js
+++ client/src/Movies/CommentCard.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import { AiOutlineCloseSquare } from "react-icons/ai";
-import '../Movies/CommentCard.css';
-
+import "../Movies/CommentCard.css";
 
 const CommentCard = ({ comment }) => {
-
-
-  const {id} = comment
-
+  const { id } = comment;
 
   return (
-
-
-    <div className='comment-card'>
-        <div className='card-top'>
-        <p className="card__exit"> <AiOutlineCloseSquare size={15} /></p>
+    <NavLink to={`/comments/${id}`}>
+      <div className="comment-card">
+        <div className="card-top">
+          <p className="card__exit">
+            {" "}
+            <AiOutlineCloseSquare size={15} />
+          </p>
         </div>
-        <NavLink to={`/comments/${id}`}><p className="comment">{comment.content}</p></NavLink>
-    </div>
-  )
-}
+        <p className="comment">{comment.content}</p>
+      </div>
+    </NavLink>
+  );
+};
 
-export default CommentCard
+export default CommentCard;
