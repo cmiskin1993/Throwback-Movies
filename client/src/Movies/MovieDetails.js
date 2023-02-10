155a0a1bb6f58f5aa1c5b76d877d7488f22c420d
Carrie Miskin <clove40@gmail.com>
2023-02-10 10:14:51 -0500

    debugged

--- client/src/Movies/MovieDetails.js
+++ client/src/Movies/MovieDetails.js
@@ -4,7 +4,6 @@ import './MovieDetails.css'
 import '../Sessions/Form.css'
 import '../Movies/CommentCard.css'
 import CommentCard from './CommentCard'
-import RichTextEditor from '../TextEditor/RichTextEditor'
 
 
 
