aff2bc1f5ddc6e50a0c791c85fb8f60fd0c7fe6e
Carrie Miskin <clove40@gmail.com>
2023-02-22 14:24:12 -0500

    updated seed file for easier process

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
     has_many :comments
+    has_many :likes
+    
     # validates :title, presence: true, uniqueness: {case_sensitive: false}
     # validates :genre, presence: true
     # validates :description, presence: true, uniqueness: {case_sensitive: false}
