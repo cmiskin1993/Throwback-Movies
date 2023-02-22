f9fad86893a8d42037fbef2fc72de52481a18a6a
Carrie Miskin <clove40@gmail.com>
2023-02-22 14:25:46 -0500

    updated movie model to count likes and validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,9 @@
 class Movie < ApplicationRecord
     has_many :comments
     has_many :likes
-    
-    # validates :title, presence: true, uniqueness: {case_sensitive: false}
-    # validates :genre, presence: true
-    # validates :description, presence: true, uniqueness: {case_sensitive: false}
-    # validates :image, presence: true, uniqueness: {case_sensitive: false}
+
+    validates :title, presence: true, uniqueness: {case_sensitive: false}
+    validates :genre, presence: true
+    validates :description, presence: true, uniqueness: {case_sensitive: false}
+    validates :image, presence: true, uniqueness: {case_sensitive: false}
 end
