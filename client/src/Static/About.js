f4654f9a0d3bc8a7272d9305b90272fbe5ed6c0f
Carrie Miskin <clove40@gmail.com>
2023-03-19 17:02:30 -0400

    added about page

--- client/src/Static/About.js
+++ client/src/Static/About.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <h2> Let us tell you a little bit about ourselves</h2>
+      <p>
+        Are you a 90's baby or just love 90's Y2k nostalgia? Then you are going
+        to love Throwback Movies. Whether you have a movie in mind or you are
+        looking for some movie recs we got you covered. Plus check out and see
+        what other 90's babies are saying about the movies if it really hits the
+        nostalgia spot or is it a flop that you somehow forgot about. Enjoy!
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
