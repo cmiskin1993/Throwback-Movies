85b9ffcffd6768faa5c2e7d4152473c26b0e4b9d
Carrie Miskin <clove40@gmail.com>
2023-03-19 17:13:03 -0400

    Added about to navbar before logged in and updated README

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Phase 5 Project
+
 ## Throwback Movies
+
 Carrie Nestel (Miskin)
 
 ### Overview
 
+        Are you a 90's baby or just love 90's Y2k nostalgia? Then you are going
+        to love Throwback Movies. Whether you have a movie in mind or you are
+        looking for some movie recs we got you covered. Plus check out and see
+        what other 90's babies are saying about the movies if it really hits the
+        nostalgia spot or is it a flop that you somehow forgot about. Enjoy!
+
 ### Requirements
+
 - Ruby 2.7.4
 - NodeJS (v16), and npm
 - Postgresql
@@ -19,13 +28,12 @@ Carrie Nestel (Miskin)
 - bundle install
 - npm install --prefix client
 - rails db:create (is is creating the database)
-- then run rails db:migrate 
+- then run rails db:migrate
 - you can then uncomment the seeded data that has already been created in the seeds file under /db/seeds.rb and then run rails db:seed (this will add some dummy data into the database so you have a starting point)
 
+You can now run the application:
 
-You can now run the application: 
-
-- rails s: to get your backend server running 
+- rails s: to get your backend server running
   - to check that the backend is up and running you should see the data here [http://localhost:3000]
 - npm start --prefix client: to run the react application
-  - the app should be running on [http://localhost:4000] 
+  - the app should be running on [http://localhost:4000]
--- client/src/Navigation/Navbar.js
+++ client/src/Navigation/Navbar.js
@@ -1,29 +1,24 @@
 import React from "react";
-import {NavLink, useNavigate} from 'react-router-dom';
-import '../Navigation/Navbar.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../Actions/sessions';
-
-
+import { NavLink, useNavigate } from "react-router-dom";
+import "../Navigation/Navbar.css";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../Actions/sessions";
 
 const Navbar = () => {
-
-  const currentUser = useSelector(state => state.sessions.currentUser)
-  const updateUser = useSelector(state => state.sessions.updateUser)
+  const currentUser = useSelector((state) => state.sessions.currentUser);
+  const updateUser = useSelector((state) => state.sessions.updateUser);
 
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
 
-
-  const handleLogOut = e => {
+  const handleLogOut = (e) => {
     e.preventDefault();
 
-    dispatch(logout(navigate))
-    
-    console.log('logged out')
+    dispatch(logout(navigate));
 
-  }
+    console.log("logged out");
+  };
 
   // const handleLogOut = () => {
   //   fetch('/logout',{
@@ -33,36 +28,57 @@ const Navbar = () => {
   //       navigate("/")
   // }
 
+  console.log("updateUser", updateUser);
+  console.log("currentUser", currentUser);
 
-console.log('updateUser', updateUser)
-console.log('currentUser', currentUser)
-
-  if( updateUser ) {
-  return (
-    <div className="navBar">
-          <ul>
-            <h4 className='user-name'> {currentUser.name} </h4>
-            <li><NavLink to="/" id="logo" className="active-link" >Welcome </NavLink></li>
-            <li><NavLink to="/movies" > Show All Movies</NavLink></li>
-            <li><NavLink to="/about" > About</NavLink></li>
-            <button  className='logout-button' onClick={handleLogOut}>Log Out</button>          
-          </ul>
-    </div>
-    )
+  if (updateUser) {
+    return (
+      <div className="navBar">
+        <ul>
+          <h4 className="user-name"> {currentUser.name} </h4>
+          <li>
+            <NavLink to="/" id="logo" className="active-link">
+              Welcome{" "}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies"> Show All Movies</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about"> About</NavLink>
+          </li>
+          <button className="logout-button" onClick={handleLogOut}>
+            Log Out
+          </button>
+        </ul>
+      </div>
+    );
   }
 
-
   return (
     <div className="navBar">
-        <ul>
-          <li><NavLink to="/" id="logo" className="active-link" >Welcome</NavLink></li>
-
-          <li><NavLink to='/signup' className="active-link">Sign Up</NavLink></li>
-          <li><NavLink to='/login' className="active-link">Login</NavLink></li>
-        </ul>
+      <ul>
+        <li>
+          <NavLink to="/" id="logo" className="active-link">
+            Welcome
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/signup" className="active-link">
+            Sign Up
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className="active-link">
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about"> About</NavLink>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
