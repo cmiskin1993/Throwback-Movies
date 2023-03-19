20a12555b37104f6ae293be96aa2ebbb8e514ec8
Carrie Miskin <clove40@gmail.com>
2023-03-19 16:14:15 -0400

    move fallback to top of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,15 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
   resources :users, only: [:show, :create, :destroy]
-
   resources :movies, only: [:index, :create, :show] do
     resources :comments, shallow: true
   end
-
   resources :likes, only: [:index, :create]
-
-
 
   post '/login', to: 'sessions#create'
   get '/authorized_user', to:'users#show'
   delete '/logout', to: 'sessions#destroy'
-
-  
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
