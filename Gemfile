d6067487c0d6fcad75a7245680de0689cce6ca51
Carrie Miskin <clove40@gmail.com>
2023-04-01 21:54:30 -0400

    added validates image url is reachable

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 gem "validate_url"
 
+gem "httparty"
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,17 @@ class Movie < ApplicationRecord
     validates :genre, presence: true, length: { minimum: 3 }
     validates :description, presence: true, length: { minimum: 30 }
     validates :image, presence: true, url: true
+    validate :image_url_is_reachable,  :if => Proc.new { |movie| movie.errors["image"].empty? }
+    
+    def image_url_is_reachable
+        response = HTTParty.get(image)
+        puts "image url resp: #{response.code}"
+        puts "image url type: #{response.headers['content-type']}"
+
+        success = response.code == 200
+        is_image = response.headers['content-type'].include?('image/')
+        if !success || !is_image 
+            errors.add(:image, "image is unreachable")
+        end
+    end
 end
